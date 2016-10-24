e1463f07a616a45bd1511e5c134c20d7a681b972
liznitopor <liznitopor48@gmail.com>
2016-10-24 15:36:39 +0300

    Я переделал окончательно клавиатуру. Добавил специальніе символы. Добавил изменения строки при нажатии на кнопки.

--- keyboard.cpp
+++ keyboard.cpp
@@ -9,6 +9,7 @@ Keyboard::Keyboard(QWidget *parent) :
     ui->setupUi(this);
     lang = 1;
     capslock = 0;
+    specSymbol = 0;
 }
 
 Keyboard::~Keyboard()
@@ -172,6 +173,7 @@ void Keyboard::on_pushButton_40_clicked()
     }
     Caps();
 }
+
 void Keyboard::Caps(){
     if(lang == 0 && capslock == 1){
 
@@ -286,4 +288,409 @@ void Keyboard::Caps(){
     }
     if(capslock == 0)
         changeKeyLang();
+}
+
+//При нажатии на спец символы
+void Keyboard::on_pushButton_50_clicked()
+{
+    if(specSymbol == 0){//показать спец символы и скрыть остальные
+        ui->pushButton->setText("-");
+        ui->pushButton_2->setText("+");
+        ui->pushButton_3->setText(":");
+        ui->pushButton_4->setText(",");
+        ui->pushButton_5->setText(".");
+        ui->pushButton_6->setText("_");
+        ui->pushButton_7->setText("/");
+        ui->pushButton_8->setText("|");
+        ui->pushButton_9->setText("?");
+        ui->pushButton_10->setText("@");
+        ui->pushButton_11->setText("{");
+        ui->pushButton_12->setText("}");
+        ui->pushButton_14->setText("[");
+        ui->pushButton_15->setText("]");
+        ui->pushButton_16->setText("<");
+        ui->pushButton_17->setText(">");
+
+        ui->pushButton_18->setText("");   //убираю текст
+        ui->pushButton_19->setText("");
+        ui->pushButton_20->setText("");
+        ui->pushButton_21->setText("");
+        ui->pushButton_22->setText("");
+        ui->pushButton_23->setText("");
+        ui->pushButton_24->setText("");
+        ui->pushButton_25->setText("");
+        ui->pushButton_26->setText("");
+        ui->pushButton_27->setText("");
+        ui->pushButton_28->setText("");
+        ui->pushButton_29->setText("");
+        ui->pushButton_30->setText("");
+        ui->pushButton_31->setText("");
+        ui->pushButton_32->setText("");
+        ui->pushButton_33->setText("");
+        ui->pushButton_34->setText("");
+        ui->pushButton_35->setText("");
+        ui->pushButton_36->setText("");
+        ui->pushButton_37->setText("");
+        ui->pushButton_38->setText("");
+        ui->pushButton_39->setText("");
+        ui->pushButton_41->setText("");
+        ui->pushButton_42->setText("");
+        ui->pushButton_43->setText("");
+        ui->pushButton_44->setText("");
+        ui->pushButton_45->setText("");
+        ui->pushButton_46->setText("");
+        ui->pushButton_47->setText("");
+        ui->pushButton_48->setText("");
+        ui->pushButton_49->setText("");
+
+        ui->pushButton_18->setEnabled(false);   //делаю недоступными
+        ui->pushButton_19->setEnabled(false);
+        ui->pushButton_20->setEnabled(false);
+        ui->pushButton_21->setEnabled(false);
+        ui->pushButton_22->setEnabled(false);
+        ui->pushButton_23->setEnabled(false);
+        ui->pushButton_24->setEnabled(false);
+        ui->pushButton_25->setEnabled(false);
+        ui->pushButton_26->setEnabled(false);
+        ui->pushButton_27->setEnabled(false);
+        ui->pushButton_28->setEnabled(false);
+        ui->pushButton_29->setEnabled(false);
+        ui->pushButton_30->setEnabled(false);
+        ui->pushButton_31->setEnabled(false);
+        ui->pushButton_32->setEnabled(false);
+        ui->pushButton_33->setEnabled(false);
+        ui->pushButton_34->setEnabled(false);
+        ui->pushButton_35->setEnabled(false);
+        ui->pushButton_36->setEnabled(false);
+        ui->pushButton_37->setEnabled(false);
+        ui->pushButton_38->setEnabled(false);
+        ui->pushButton_39->setEnabled(false);
+        ui->pushButton_41->setEnabled(false);
+        ui->pushButton_42->setEnabled(false);
+        ui->pushButton_43->setEnabled(false);
+        ui->pushButton_44->setEnabled(false);
+        ui->pushButton_45->setEnabled(false);
+        ui->pushButton_46->setEnabled(false);
+        ui->pushButton_47->setEnabled(false);
+        ui->pushButton_48->setEnabled(false);
+        ui->pushButton_49->setEnabled(false);
+
+        ui->pushButton_40->setEnabled(false);//убираю капс и языки
+        ui->pushButton_51->setEnabled(false);
+        ui->pushButton_53->setEnabled(false);
+        ui->pushButton_54->setEnabled(false);
+        ui->pushButton_55->setEnabled(false);
+
+        specSymbol++;
+        return;
+    }
+
+    if(specSymbol == 1){ // при выходе из режима спец символов
+
+        ui->pushButton_18->setEnabled(true);   //делаю доступными
+        ui->pushButton_19->setEnabled(true);
+        ui->pushButton_20->setEnabled(true);
+        ui->pushButton_21->setEnabled(true);
+        ui->pushButton_22->setEnabled(true);
+        ui->pushButton_23->setEnabled(true);
+        ui->pushButton_24->setEnabled(true);
+        ui->pushButton_25->setEnabled(true);
+        ui->pushButton_26->setEnabled(true);
+        ui->pushButton_27->setEnabled(true);
+        ui->pushButton_28->setEnabled(true);
+        ui->pushButton_29->setEnabled(true);
+        ui->pushButton_30->setEnabled(true);
+        ui->pushButton_31->setEnabled(true);
+        ui->pushButton_32->setEnabled(true);
+        ui->pushButton_33->setEnabled(true);
+        ui->pushButton_34->setEnabled(true);
+        ui->pushButton_35->setEnabled(true);
+        ui->pushButton_36->setEnabled(true);
+        ui->pushButton_37->setEnabled(true);
+        ui->pushButton_38->setEnabled(true);
+        ui->pushButton_39->setEnabled(true);
+        ui->pushButton_41->setEnabled(true);
+        ui->pushButton_42->setEnabled(true);
+        ui->pushButton_43->setEnabled(true);
+        ui->pushButton_44->setEnabled(true);
+        ui->pushButton_45->setEnabled(true);
+        ui->pushButton_46->setEnabled(true);
+        ui->pushButton_47->setEnabled(true);
+        ui->pushButton_48->setEnabled(true);
+        ui->pushButton_49->setEnabled(true);
+
+        ui->pushButton_40->setEnabled(true);//возвращаю капс и языки
+        ui->pushButton_51->setEnabled(true);
+        ui->pushButton_53->setEnabled(true);
+        ui->pushButton_54->setEnabled(true);
+        ui->pushButton_55->setEnabled(true);
+
+        changeKeyLang();
+
+        ui->pushButton_2->setText("1");
+        ui->pushButton_3->setText("2");
+        ui->pushButton_4->setText("3");
+        ui->pushButton_5->setText("4");
+        ui->pushButton_6->setText("5");
+        ui->pushButton_7->setText("6");
+        ui->pushButton_8->setText("7");
+        ui->pushButton_9->setText("8");
+        ui->pushButton_10->setText("9");
+        ui->pushButton_11->setText("0");
+        ui->pushButton_12->setText("-");
+        ui->pushButton_26->setText("_");
+        ui->pushButton_38->setText(".");
+        ui->pushButton_39->setText(",");
+
+
+        specSymbol--;
+    }
+}
+
+void Keyboard::on_pushButton_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton->text());
+}
+
+void Keyboard::on_pushButton_2_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_2->text());
+}
+
+void Keyboard::on_pushButton_3_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_3->text());
+}
+
+void Keyboard::on_pushButton_4_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_4->text());
+}
+
+void Keyboard::on_pushButton_5_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_5->text());
+}
+
+void Keyboard::on_pushButton_7_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_7->text());
+}
+
+void Keyboard::on_pushButton_6_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_6->text());
+}
+
+void Keyboard::on_pushButton_8_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_8->text());
+}
+
+void Keyboard::on_pushButton_9_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_9->text());
+}
+
+void Keyboard::on_pushButton_10_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_10->text());
+}
+
+void Keyboard::on_pushButton_11_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_11->text());
+}
+
+void Keyboard::on_pushButton_12_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_12->text());
+}
+
+void Keyboard::on_pushButton_14_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_14->text());
+}
+
+void Keyboard::on_pushButton_15_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_15->text());
+}
+
+void Keyboard::on_pushButton_16_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_16->text());
+}
+
+void Keyboard::on_pushButton_17_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_17->text());
+}
+
+void Keyboard::on_pushButton_18_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_18->text());
+}
+
+void Keyboard::on_pushButton_19_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_19->text());
+}
+
+void Keyboard::on_pushButton_20_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_20->text());
+}
+
+void Keyboard::on_pushButton_21_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_21->text());
+}
+
+void Keyboard::on_pushButton_22_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_22->text());
+}
+
+void Keyboard::on_pushButton_23_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_23->text());
+}
+
+void Keyboard::on_pushButton_24_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_24->text());
+}
+
+void Keyboard::on_pushButton_25_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_25->text());
+}
+
+void Keyboard::on_pushButton_26_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_26->text());
+}
+
+void Keyboard::on_pushButton_27_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_27->text());
+}
+
+void Keyboard::on_pushButton_28_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_28->text());
+}
+
+void Keyboard::on_pushButton_29_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_29->text());
+}
+
+void Keyboard::on_pushButton_30_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_30->text());
+}
+
+void Keyboard::on_pushButton_31_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_31->text());
+}
+
+void Keyboard::on_pushButton_32_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_32->text());
+}
+
+void Keyboard::on_pushButton_33_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_33->text());
+}
+
+void Keyboard::on_pushButton_34_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_34->text());
+}
+
+void Keyboard::on_pushButton_35_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_35->text());
+}
+
+void Keyboard::on_pushButton_36_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_36->text());
+}
+
+void Keyboard::on_pushButton_37_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_37->text());
+}
+
+void Keyboard::on_pushButton_38_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_38->text());
+}
+
+void Keyboard::on_pushButton_39_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_39->text());
+}
+
+void Keyboard::on_pushButton_41_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_41->text());
+}
+
+void Keyboard::on_pushButton_42_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_42->text());
+}
+
+void Keyboard::on_pushButton_43_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_43->text());
+}
+
+void Keyboard::on_pushButton_44_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_44->text());
+}
+
+void Keyboard::on_pushButton_45_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_45->text());
+}
+
+void Keyboard::on_pushButton_46_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_46->text());
+}
+
+void Keyboard::on_pushButton_47_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_47->text());
+}
+
+void Keyboard::on_pushButton_48_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_48->text());
+}
+
+void Keyboard::on_pushButton_49_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + ui->pushButton_49->text());
+}
+
+void Keyboard::on_pushButton_52_clicked()
+{
+    ui->editBox->setText(ui->editBox->text() + " ");
+}
+
+void Keyboard::on_pushButton_13_clicked()
+{
+    QString str;
+    str = ui->editBox->text();
+    str.remove(str.length()-1, 1);
+    ui->editBox->setText(str);
 }
--- keyboard.h
+++ keyboard.h
@@ -2,6 +2,8 @@
 #define KEYBOARD_H
 
 #include <QDialog>
+#include <qstring.h>
+#include <QDebug>
 
 namespace Ui {
 class Keyboard;
@@ -16,6 +18,7 @@ public:
     ~Keyboard();
     int lang;
     int capslock;
+    int specSymbol;
 private slots:
     void on_pushButton_53_clicked();
 
@@ -24,6 +27,106 @@ private slots:
     void on_pushButton_55_clicked();
 
     void on_pushButton_40_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_10_clicked();
+
+    void on_pushButton_11_clicked();
+
+    void on_pushButton_12_clicked();
+
+    void on_pushButton_14_clicked();
+
+    void on_pushButton_15_clicked();
+
+    void on_pushButton_16_clicked();
+
+    void on_pushButton_17_clicked();
+
+    void on_pushButton_18_clicked();
+
+    void on_pushButton_19_clicked();
+
+    void on_pushButton_20_clicked();
+
+    void on_pushButton_21_clicked();
+
+    void on_pushButton_22_clicked();
+
+    void on_pushButton_23_clicked();
+
+    void on_pushButton_24_clicked();
+
+    void on_pushButton_25_clicked();
+
+    void on_pushButton_50_clicked();
+
+    void on_pushButton_26_clicked();
+
+    void on_pushButton_27_clicked();
+
+    void on_pushButton_28_clicked();
+
+    void on_pushButton_29_clicked();
+
+    void on_pushButton_30_clicked();
+
+    void on_pushButton_31_clicked();
+
+    void on_pushButton_32_clicked();
+
+    void on_pushButton_33_clicked();
+
+    void on_pushButton_34_clicked();
+
+    void on_pushButton_35_clicked();
+
+    void on_pushButton_36_clicked();
+
+    void on_pushButton_37_clicked();
+
+    void on_pushButton_38_clicked();
+
+    void on_pushButton_39_clicked();
+
+    void on_pushButton_41_clicked();
+
+    void on_pushButton_42_clicked();
+
+    void on_pushButton_43_clicked();
+
+    void on_pushButton_44_clicked();
+
+    void on_pushButton_45_clicked();
+
+    void on_pushButton_46_clicked();
+
+    void on_pushButton_47_clicked();
+
+    void on_pushButton_48_clicked();
+
+    void on_pushButton_49_clicked();
+
+    void on_pushButton_52_clicked();
+
+    void on_pushButton_13_clicked();
 
 private:
     Ui::Keyboard *ui;
